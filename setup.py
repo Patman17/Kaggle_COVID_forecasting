8c110578b807ed2ca8a02e76fcc3d9c0ad7530cc
Patrick Ly <patrick.ly17@gmail.com>
2020-04-09 11:06:16 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Kaggle competition to make a model to predict weekly forecasting of COVID 19',
+    author='Patrick Ly',
+    license='MIT',
+)
